e8729eb506bc79204d7e7cba9329b99d2c5a0327
Rawle Juglal <rawledjuglal@gmail.com>
2016-06-11 00:04:25 +0000

    finished flipboard site

--- public/javascripts/codecademy/flipBoard.js
+++ public/javascripts/codecademy/flipBoard.js
@@ -1,24 +1,52 @@
-/** Document Ready Functions **/
-/********************************************************************/
+//Rawle JavaScript Document
 
-$( document ).ready(function() {
-    getHeight();
-    
-    $(window).on('resize', function() {
-        getHeight();
-    });
-});
-
-/** Reusable Functions **/
-/********************************************************************/
-
-function getHeight(){
-    var windowHeight = $(window).height();
-    var headingHeight = parseInt(windowHeight-50)+'px';
-    
-    initHeight(headingHeight);
+var main = function(){
+    $('.arrow-next').click(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        
+        if(nextSlide.length === 0)
+            {
+                nextSlide = $('.slide').first();
+            }
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+        
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+        
+        if(nextDot.length === 0)
+            {
+                nextDot = $('.dot').first();
+            }
+            
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+        });
+        
+        $('.arrow-prev').click(function(){
+            var currentSlide = $('.active-slide');
+            var prevSlide = currentSlide.prev();
+            
+            if(prevSlide.length === 0)
+                    {
+                        prevSlide = $('.slide').last();
+                    }
+                    
+            currentSlide.fadeOut(600).removeClass('active-slide');
+            prevSlide.fadeIn(600).addClass('active-slide');
+            
+            var currentDot = $('.active-dot');
+            var prevDot = currentDot.prev();
+        
+            if(prevDot.length === 0)
+                {
+                    prevDot = $('.dot').last();
+                }
+            
+            currentDot.removeClass('active-dot');
+            prevDot.addClass('active-dot');
+            });
 }
 
-function initHeight(num){
-    $('.carousel-inner').css('height',num);
-}
+$(document).ready(main);
